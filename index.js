53fd015c6e1dd02aa2a2ae0c9b97106cc7628504
Gonzalo Gabriel Jesús Tapie <135050916+tapiedevel@users.noreply.github.com>
2026-01-17 01:05:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.use((req,res,next)=>{
+    res.json({'mensaje':'Hola Middleware'});
+});
+
+app.get('/', (req,res)=>{
+    res.send('Bienvenidos a nuestra API REST');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
