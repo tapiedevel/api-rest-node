5f583def5e694834c3402470c1d3d2cbb664a8ef
Gonzalo Gabriel Jesús Tapie <gonzalogabrieltapie@gmail.com>
2026-01-18 16:47:28 -0300

    Add Middleware

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import express from 'express';
 const app = express();
 
 app.use((req,res,next)=>{
-    res.json({'mensaje':'Hola Middleware'});
+    //res.json({'mensaje':'API en mantenimiento'});
+    console.log(req.method);
+    next();
 });
 
 app.get('/', (req,res)=>{
-    res.send('Bienvenidos a nuestra API REST!');
+    res.json({'mensaje':'Bienvenidos a nuestra API REST!'});
 });
+
+import notFound from './src/middlewares/not-found.js';
+app.use(notFound);
 
 const PORT = 3000;
 
